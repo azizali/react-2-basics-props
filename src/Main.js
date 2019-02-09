a747f3d13bd34f663bd8be9a235ba1aefbcf2ba4
Aziz Ali <aziz@ilovecoding.org>
2019-02-09 17:51:46 -0600

    List of cars - part 1

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Button from './Button'
+import List from './List'
 
 function Main(){
   return (
     <div className="border p-5">
       <div className="highlight">Main content</div>
       <Button value="Click me" />
+      <List />
     </div>
   )
 }
